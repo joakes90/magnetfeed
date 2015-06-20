6c9b21e006469963fd86cea51a0c755c5d07cd35
Justin Oakes <justin@justinoak.es>
2015-06-20 00:02:38 -0600

    Initial Commit

---
+++ Magnet Feed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Magnet Feed
+//
+//  Created by Justin Oakes on 6/20/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Magnet Feed/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Magnet Feed
+//
+//  Created by Justin Oakes on 6/20/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
