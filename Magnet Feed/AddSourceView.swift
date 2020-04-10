6adab1921f690e514facaf963736ec35ae379ffc
Justin Oakes <justin.t.oakes@gmail.com>
2020-04-10 14:10:49 -0500

    added auto detecting pasteboard URLs

--- Magnet Feed/AddSourceView.swift
+++ Magnet Feed/AddSourceView.swift
@@ -19,13 +19,29 @@ class AddSourceView: NSViewController {
         // Do view setup here.
     }
 
+    
     override func viewWillAppear() {
         super.viewWillAppear()
+        fixAppearance()
+        if let url = pasteboardURL() {
+            textField.stringValue = url
+        }
+    }
+
+    fileprivate func fixAppearance() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             // Hack around text field vibrancy bug
             let dark = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
             self.textField.becomeFirstResponder()
             self.textField.appearance = NSAppearance(named: dark ? .darkAqua : .aqua)
         }
+    }
+    
+    fileprivate func pasteboardURL() -> String? {
+        if let string = NSPasteboard.general.string(forType: .string),
+            let _ = URL(string: string) {
+            return string
+        }
+        return nil
     }
 }
